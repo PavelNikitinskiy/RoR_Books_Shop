58e2d7c559dd3cb0c5ac46779699a0314177794f
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-03 16:17:15 +0200

    127

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,7 @@ class Product < ActiveRecord::Base
               message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
                       }
 
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -52,4 +52,16 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+  test "should get products index" do
+    get :index
+    assert_response :success
+    assert_select '.products .list_description', minimum: 3
+    assert_select '.products .list_description dl dt', 'Programming Ruby 1.9'
+    assert_select '.products .list_description dl dd', /.{1,80}/
+    assert_select '.products .list_actions', minimum: 3
+    ['Show', 'Edit', 'Destroy'].each do |action|
+      assert_select '.products .list_actions a', action
+    end
+    assert_select 'a', 'New Product'
+  end
 end
