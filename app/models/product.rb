0c262dd9698b9aaadf9fbad29447bc3ed70ef020
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-04 22:17:59 +0200

    Merge branch 'master' of https://bitbucket.org/NikitinskiyPavel/depot
    Conflicts:
    	.idea/workspace.xml

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,30 @@
 class Product < ActiveRecord::Base
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence:true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness:true
+  validates :title, uniqueness:true, length: {
+                minimum: 10,
+                message: "must be at least ten characters."
+                  }
   validates :image_url, allow_blank: true, format: {
               with: %r{\.(gif|jpg|png)\Z}i,
               message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
                       }
 
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  get 'store/index'
+
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root to: 'store#index', as: 'store'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
