e80c20d4f5122b4fd834290d959fde21fedaa40b
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-05 15:48:36 +0200

    135

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,23 +3,24 @@ require 'test_helper'
 class ProductTest < ActiveSupport::TestCase
   fixtures :products
   test "product attributes must not be empty" do
-    product = Product.new()
+    product = Product.new
     assert product.invalid?
     assert product.errors[:title].any?
     assert product.errors[:description].any?
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
+
   test "product price must be positive" do
     product = Product.new(title: "My book", description:"yy", image_url: "zzz.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
-                product.errors[:price]
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
-                 product.errors[:price]
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
     product.price = 1
     assert product.valid?
   end
