f591b10c111ca9ae5c88a968ba3dcd09906b80f1
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-06 14:26:14 +0200

    139

---
+++ db/migrate/20151106090356_combine_items_in_cart.rb
@@ -0,0 +1,31 @@
+class CombineItemsInCart < ActiveRecord::Migration
+  def change
+    reversible do |change|
+      change.up do
+        Cart.all.each do |cart|
+          # подсчет количества каждого товара в корзине
+          sums = cart.line_items.group(:product_id).sum(:quantity)
+          sums.each do |product_id, quantity|
+            if quantity > 1
+              # удаление отдельных записей
+              cart.line_items.where(product_id: product_id).delete_all
+              # замена одной записью
+              item = cart.line_items.build(product_id: product_id)
+              item.quantity = quantity
+              item.save!
+            end
+          end
+        end
+      end
+      change.down do
+        LineItem.where("quantity>1").each do |line_item|
+          line_item.quantity.times do
+            LineItem.create cart_id: line_item.cart_id,
+                            product_id: line_item.product_id, quantity: 1
+          end
+          line_item.destroy
+        end
+      end
+    end
+  end
+end
