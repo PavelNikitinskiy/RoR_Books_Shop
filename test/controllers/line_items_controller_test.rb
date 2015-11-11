c5f88e277f859eeecb24f010c99ac5001980b1c1
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-11 23:30:04 +0200

    173 on test

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -20,7 +20,7 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_difference('LineItem.count') do
       post :create, product_id: products(:ruby).id
     end
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
   end
 
   test "should show line_item" do
@@ -42,7 +42,6 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_difference('LineItem.count', -1) do
       delete :destroy, id: @line_item
     end
-
-    assert_redirected_to line_items_path
+    assert_redirected_to store_url
   end
 end
