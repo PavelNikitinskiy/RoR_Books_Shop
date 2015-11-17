86c0093d62728fc808362aac4b9441d4434acc64
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-17 23:21:37 +0200

    193 home work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -75,6 +75,26 @@ class ProductsController < ApplicationController
     if stale?(@latest_order)
       respond_to do |format|
         format.atom
+        format.html
+        format.json {render json: @product.to_json(include: :orders)}
+        format.xml { render( :xml => @product.to_xml(
+                               :only => [ :title, :updated_at ],
+                               :skip_types => true,
+                               :include => {
+                                 :orders => {
+                                   :except => [ :created_at, :updated_at ],
+                                   :skip_types => true,
+                                   :include => {
+                                     :line_items => {
+                                       :skip_types => true,
+                                       :except => [ :created_at, :updated_at, :cart_id, :order_id ]
+                                     }
+                                   }
+                                 }
+                               }
+                                                    )
+                              )
+        }
       end
     end
   end
