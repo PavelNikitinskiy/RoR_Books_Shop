9a0db3d9da9e9b68da1c41f10bf6c7bd416dd5b2
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-23 12:59:56 +0200

    200

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122132538) do
+ActiveRecord::Schema.define(version: 20151120082935) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
